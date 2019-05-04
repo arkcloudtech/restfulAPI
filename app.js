445bd1b5f5ed9ea7d9c5dbe3defc4f1f2f5eb354
kenovate2018 <36251224+kenovate2018@users.noreply.github.com>
2019-05-04 16:04:42 -0700

    add nodemon to keep server running, added global error handling

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
+// middleware
+app.use(morgan('dev'));
+
+// routes
 const userRoutes = require('./api/routes/users');
 const medRecRoutes = require('./api/routes/medrecs');
 const stateIdRoutes = require('./api/routes/stateids');
 
+// hand-offs
 app.use('/users', userRoutes);
 app.use('/medrecs', medRecRoutes);
 app.use('/stateids', stateIdRoutes);
 
-app.use('/',(req, res, next)=>{
-    res.status(200).json({
-        message: "collect all route"
+// catch any unrouted
+app.use((req, res, next)=>{
+    const error = new Error('Invalid Path');
+    error.status = (404);
+    next(error);
+});
+
+// errors caused by anything (even non-route related)
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
     });
 });
 
