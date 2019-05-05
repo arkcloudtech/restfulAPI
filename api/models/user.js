662b96f04dc9a6d2ec75d42a2f50aa43efaabaa6
kenovate2018 <36251224+kenovate2018@users.noreply.github.com>
2019-05-04 19:59:56 -0700

    fix dob field

--- api/models/user.js
+++ api/models/user.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     Name: { type: String },
     Email: { type: String, required: true },
-    dob: { type: Date, required: true },
+    Dob: { type: Date, required: true },
     UserName: { type: String, required: true },
     Password: { type: String, required: true }
 });
